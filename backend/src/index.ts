3603d187e82096d10c86531e56971fb8fb975f3e
Jimmy Lin <jlin@pdftron.com>
2024-12-11 15:17:56 -0800

    return buffer

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import pdfToWord from "./pdfToWord";
+import { PDFNet } from '@pdftron/pdfnet-node';
 
 const app: Express = express();
 const port = 3001;
@@ -9,8 +10,14 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.post("/actions", async (req: Request, res: Response) => {
-  await pdfToWord();
-  res.send('Success');
+  try {
+    const resultBuffer = await PDFNet.runWithCleanup(pdfToWord, 'demo:1683842987815:7dd33b160300000000121d415dc2081c7eab9515b4cb8a5de30b5b8157');
+    
+    res.send({ resultBuffer: resultBuffer.toString() });
+    PDFNet.shutdown();
+  } catch (error) {
+    console.log("🚀 -> PDFNet -> error:", JSON.stringify(error))
+  }
 });
 
 app.listen(port, () => {
--- backend/src/pdfToWord.ts
+++ backend/src/pdfToWord.ts
@@ -1,29 +1,23 @@
-
 import { PDFNet } from '@pdftron/pdfnet-node';
+import fs from 'fs';
 
 export default async function pdfToWord() {
-  const main = async () => {
-    try {
-      await PDFNet.addResourceSearchPath('lib/StructuredOutputMac');
-  
-      if (!(await PDFNet.StructuredOutputModule.isModuleAvailable())) {
-        throw new Error('PDF to WORD Error - resource path not found');
-      }
-      // const PDFDoc = await PDFNet.PDFDoc.createFromBuffer(s3FileBuffer.buffer);
-      const PDFDoc = await PDFNet.PDFDoc.createFromURL('https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf');
-      const wordOutputOptions = new PDFNet.Convert.WordOutputOptions();
-      await PDFNet.Convert.toWord(PDFDoc, 'newfile.doc', wordOutputOptions);
-      
-    } catch (error) {
-      console.log("🚀 -> main -> error:", error)
-    }
-  };
-
   try {
-    await PDFNet.runWithCleanup(main, 'demo:1683842987815:7dd33b160300000000121d415dc2081c7eab9515b4cb8a5de30b5b8157');
-    PDFNet.shutdown();
+    await PDFNet.addResourceSearchPath('lib/StructuredOutputMac');
+
+    if (!(await PDFNet.StructuredOutputModule.isModuleAvailable())) {
+      throw new Error('PDF to WORD Error - resource path not found');
+    }
+    // const PDFDoc = await PDFNet.PDFDoc.createFromBuffer(s3FileBuffer.buffer);
+    const PDFDoc = await PDFNet.PDFDoc.createFromURL(
+      'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf'
+    );
+
+    const wordOutputOptions = new PDFNet.Convert.WordOutputOptions();
+    await PDFNet.Convert.toWord(PDFDoc, 'newfile.doc', wordOutputOptions);
+    const buffer = fs.readFileSync('newfile.doc');
+    return buffer;
   } catch (error) {
-    console.log("🚀 -> pdfToWord -> error:", JSON.stringify(error))
+    console.log('🚀 -> main -> error:', error);
   }
 }
-
