a5fd9bfb5cbb2ae07c8e63c9ed53cae880481a2f
Levi Halvorson <lhalvorson@apryse.com>
2024-12-11 12:26:00 -0700

    add assets

--- src/components/Webviewer.tsx
+++ src/components/Webviewer.tsx
@@ -1,22 +1,37 @@
+'use client';
+
 import WebViewer from '@pdftron/webviewer';
+import React, { useEffect, useRef } from 'react';
 
-const viewerElement = document.getElementById('viewer');
-if (viewerElement) {
-  WebViewer.WebComponent(
-    {
-      path: '/public/webviewer',
-      licenseKey: 'demo:1710282533582:7f3fac9803000000004e3fa69682defdb4b13dab92abd526876f7191ac',
-    },
-    viewerElement
-  ).then((instance) => {
-    const { Core } = instance;
-    const { documentViewer } = Core;
-    // call methods from UI, Core, documentViewer and annotationManager as needed
+const Webviewer = () => {
+  const viewer = useRef(null);
 
-    documentViewer.addEventListener('documentLoaded', () => {
-      // call methods relating to the loaded document
+  useEffect(() => {
+    if (!viewer.current) return;
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        licenseKey: 'demo:1710282533582:7f3fac9803000000004e3fa69682defdb4b13dab92abd526876f7191ac', // sign up to get a key at https://dev.apryse.com
+        initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+      },
+      viewer.current
+    ).then((instance) => {
+      console.log('🚀 ~ ).then ~ instance:', instance);
+      // const { docViewer } = instance;
+      // you can now call WebViewer APIs here...
     });
+  }, []);
 
-    instance.UI.loadDocument('https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf');
-  });
-}
+  return (
+    <div className="MyComponent">
+      <div className="header">React sample</div>
+      <div
+        className="webviewer"
+        ref={viewer}
+        style={{ height: '100vh' }}
+      ></div>
+    </div>
+  );
+};
+
+export default Webviewer;
