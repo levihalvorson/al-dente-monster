70397ede08a44fa22d8209c27efc6a4e089de862
Jimmy Lin <jlin@pdftron.com>
2024-12-12 11:24:36 -0800

    add body parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 import process from "./process";
 
 const app: Express = express();
 const port = 3001;
+
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
@@ -10,14 +13,18 @@ app.get("/", (req: Request, res: Response) => {
 
 app.post("/actions", async (req: Request, res: Response) => {
   try {
-    // const { files, actions } = req.body;
+    const { files, actions } = req.body;
+    console.log("🚀 -> app.post -> actions:", actions)
+    console.log("🚀 -> app.post -> files:", files)
     await process({
       files: ['file1.pdf', 'file2.pdf', 'file3.pdf'],
-      actions: ['merge', 'pdfToWord', 'toPdf'],
+      actions: ['merge', 'pdfToWord'],
     })
     res.send('Success');
   } catch (error) {
+    console.log("🚀 -> app.post -> error:", error)
     console.log("🚀 -> PDFNet -> error:", JSON.stringify(error))
+    res.status(500).send('Error');
   }
 });
 
